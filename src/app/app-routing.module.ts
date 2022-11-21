e6d1fdb2c3084e170d66a2bd0275fd75c2d2dfe9
Martin-Melody <78037342+Martin-Melody@users.noreply.github.com>
2022-11-21 17:43:43 +0000

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeliveryViewComponent } from './Components/delivery-view/delivery-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:DeliveryViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeliveryViewComponent } from './Components/delivery-view/delivery-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeliveryViewComponent
   ],
   imports: [
     BrowserModule,
